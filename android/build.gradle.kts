c4118164df1d7fdbdbd700bdabaaae67adbdd169
chris_code <dianesekachrist@gmail.com>
2026-03-28 19:21:40 +0000

    Fix namespace subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,18 @@ allprojects {
     }
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            android {
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
